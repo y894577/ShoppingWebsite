774c48ebb0679fa61350c725d3a3177192a5df9f
y894577 <44811269+y894577@users.noreply.github.com>
2020-03-30 21:22:04 +0800

    refactor：optimize the structure of document
    每天只改一点点，健康996从你我做起

---
+++ application/index/validate/User.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace app\index\validate;
+
+use think\Validate;
+
+
+class User extends Validate
+{
+    protected $rule = [
+        'name' => 'require|max:25',
+        'email' => 'email',
+    ];
+
+}

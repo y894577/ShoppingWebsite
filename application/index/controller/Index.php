5942a4288cefeab057cfe2b649f801efb617bb0b
y894577 <44811269+y894577@users.noreply.github.com>
2020-04-26 13:02:59 +0800

    feat: upload user head portrait function

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -296,4 +296,19 @@ class Index extends Controller
         $model = new Order();
         $model->submitOrder($form, $address);
     }
+
+    public function getImg(Request $request)
+    {
+        $file = $request->file('image');
+
+        if ($file) {
+            $oldPath = $file->getInfo()["tmp_name"];
+            $fileName = $request->post('fileName');
+            $newPath = $_SERVER['DOCUMENT_ROOT'].'/public/static/img/user/'.$fileName.'.jpg';
+            move_uploaded_file($oldPath,$newPath);
+        } else {
+            // 上传失败获取错误信息
+            echo $file->getError();
+        }
+    }
 }
